b0cbee47360f2d23e7af57042977711eeca164df
Matt Bee <matt@todayishould.com>
2016-04-20 16:06:07 +0100

    Add initial files and initial readme docs.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # freelance-site
-Site to showcase my freelance skills to local agencies.
+
+If you're reading this you probably want to know how this site is built and evidence of my skills.  Hopefully this reassures you about some of the claims made on the page at https://todayishould.com/freelance
+
+## Technology
+
+This site was built using the following languages:
+
+* HTML
+* CSS
+* JavaScript (object oriented structure)
+* PHP (not a lot, but just to show I know something about PHP)
+
+The following technologies/services were used to streamline the generation of the above languages
+
+* bower to manage the JS dependencies
+* SASS (SCSS flavour)
+* Gulp (compiling the SASS and using auto-prefix)
+* Node (since gulp is built on node)
+* Github for storing the code (as you can see)
+* I used some Apache configuration in htaccess for some parts
+
+## Deployment
+
+As any good developer knows, there's no need to manually FTP anymore, so I deploy this site using deployhq.com, using a github hook to auto-deploy on commit.
+
+## Favoured Programs
+
+I use Sublime for my coding, all my code is purely hand written and kept to my own coding standards.
+
+## Things I would have used had I had time
+
+* Vagrant to create a local dev environment to match the live server, but not so important here as it's a simple site
+* JS Lint in Gulp to minify and lint the JS, but again very little need on this work
+
+
---
+++ index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<head>
+  
+  <title>Freelance Front End Developer - Matt Bee - Sevenaoks, Kent</title>
+
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+
+
+</head>
+<body>
+
+  <header>
+  
+    <h1>Freelance Front End Developer</h1>
+
+    <h2>Matt Bee</h2>
+
+  </header>
+
+
+  <section>
+
+    <h2>Experience</h2>
+
+    <div class="experience">
+      <article>
+        <h1>Farm Digital</h1>
+        <img src="images/farm-small.png"
+              srcset="farm-large.png 1024w, farm-medium.png 640w, farm-small.png 320w"
+              sizes="50vw"
+              alt="Farm Digital" />
+        <p>Farm Digital is a Hampshire based Strategicc, Creative and Technical agency. With clients including </p>
+        <p>I have worked with Farm Digital on a number of projects.  I support them building static websites and the occasional Django &amp; Mobile App templating work.</p>
+      </article>
+
+      <article>
+        <h1>National Theatre</h1>
+        <img src="images/nt-small.png" alt="Farm Digital.">
+        <img src="small.jpg"
+              srcset="farm-large.png 1024w, nt-medium.png 640w, farm-small.png 320w"
+              sizes="50vw"
+              alt="Farm Digital" />
+        <p>Farm Digital is a Hampshire based Strategicc, Creative and Technical agency. With clients including </p>
+        <p>I have worked with Farm Digital on a number of projects.  I support them building static websites and the occasional Django &amp; Mobile App templating work.</p>
+      </article>
+    </div>
+
+  </section>
+
+
+</body>
+</html>
